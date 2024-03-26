bd6abac962099935b3325f61ee15be4113cb4134
Danilo Mendonça <danilomendonca08@gmail.com>
2024-03-25 23:29:32 -0300

    add GPT integration and validation folder

--- src/main/java/br/com/fiap/financaspro/model/Categoria.java
+++ src/main/java/br/com/fiap/financaspro/model/Categoria.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -13,10 +12,6 @@ public class Categoria {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @NotBlank(message = "{categoria.nome.notblank}")
     private String nome;
-
-
-    private String icone = "request_quote";
+    private String icone;
 }
