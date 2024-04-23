bdd74163713a662d8341bba223a7329e8bdc0893
Danilo Mendonça <danilomendonca08@gmail.com>
2024-04-22 21:27:02 -0300

    pageable e relacionamentos

--- src/main/java/br/com/fiap/financaspro/model/Movimentacao.java
+++ src/main/java/br/com/fiap/financaspro/model/Movimentacao.java
@@ -3,18 +3,27 @@ package br.com.fiap.financaspro.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.fiap.financaspro.validation.TipoMovimentacao;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movimentacao {
     
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,8 +36,13 @@ public class Movimentacao {
     @Positive(message = "{movimentacao.valor.positive}") //validar para que o número tenha q ser obrigatoriamente positivo
     private BigDecimal valor;
 
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate data;
 
     @TipoMovimentacao
     private String tipo; //entrada ou saída de dinheiro
+
+    //cardinalidade 1 - 1 1 - n    varias movimentações podem estar na mesma categoria
+    @ManyToOne
+    public Categoria categoria;
 }
--- src/main/java/br/com/fiap/financaspro/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/fiap/financaspro/repository/MovimentacaoRepository.java
@@ -1,9 +1,24 @@
 package br.com.fiap.financaspro.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.fiap.financaspro.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+
+    List<Movimentacao> findByCategoriaNome(String categoria);
+
+    @Query("SELECT m FROM Movimentacao m WHERE MONTH(m.data) = ?1") //SQL JPQL - Java Persistence Query Language
+    List<Movimentacao> findByMes(Integer mes);
     
+    @Query("SELECT m FROM Movimentacao m WHERE MONTH(m.data) = :mes AND m.categoria.nome = :categoria") 
+    List<Movimentacao> findByCategoriaNomeAndMes(@Param("categoria")  String categoria, @Param("mes") Integer mes);
+
+    @Query("SELECT m FROM Movimentacao ORDER BY m.id LIMIT :size OFFSET :offset")
+    Page<Movimentacao> findAllPageable(@Param("offset") int offset, @Param("size") Integer size);
 }
